6f1484af918f08afcd52732b3033ce78d73a17ac
Aparna Subedi <aparna.subedi@gmail.com>
2024-03-07 08:58:18 -0500

    update

--- src/components/MarkdownInput.tsx
+++ src/components/MarkdownInput.tsx
@@ -1,30 +1,31 @@
 import styled from "styled-components";
 const InputContainer = styled.div`
-width: 50%;
-height: calc(100vh - 58.5px);
-padding: 20px;
-box-sizing: border-box;
-`
-const StyledTextArea = styled.textarea`
-width: 100%;
-height:100%;
-resize: none;
-border: none;
-padding: 10px;
-box-sizing: border-box;
-font-size: 16px;
-outline: none;
+  width: 50%;
+  height: calc(100vh - 50.5px);
+  padding: 20px;
+  box-sizing: border-box;
 `;
-interface MarkdownInputProps{
-    value: string;
-    onchange: (e:React.ChangeEvent<HTMLTextAreaElement>) => void;
+const StyledTextArea = styled.textarea`
+  width: 100%;
+  height: 100%;
+  resize: none;
+  border: 2px black;
+  padding: 10px;
+  box-sizing: border-box;
+  background-color: #e0d7ff;
+  font-size: 16px;
+  outline: none;
+`;
+interface MarkdownInputProps {
+  value: string;
+  onchange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
 }
-const MarkdownInput: React.FC<MarkdownInputProps> = ({ value, onchange}) => { return (
+const MarkdownInput: React.FC<MarkdownInputProps> = ({ value, onchange }) => {
+  return (
     <InputContainer>
-    <StyledTextArea onChange= {onchange} value={value}/>
+      <StyledTextArea onChange={onchange} value={value} />
     </InputContainer>
-);
-    return <div> MarkdownInput</div>;
-
+  );
+  return <div> MarkdownInput</div>;
 };
 export default MarkdownInput;
--- src/components/MarkdownOutput.tsx
+++ src/components/MarkdownOutput.tsx
@@ -2,13 +2,13 @@ import ReactMarkdown from "react-markdown";
 import styled from "styled-components";
 
 const OutputContainer = styled.div`
-width: 50;
-height: calc(100vh - 58.5px);
+width: 20;
+height: calc(100vh - 50.5px);
 padding: 20px;
 box-sizing: border-box;
 overflow: auto;
-border-left: 1px solid #ddd;
-background-color: #f9f9f9;
+border-left: 1px solid #1d011f;
+background-color: #E0D7FF;
 `;
 interface MarkdownOutputProps{
     markdown: string;
@@ -17,6 +17,7 @@ const MarkdownOutput: React.FC<MarkdownOutputProps> = ({markdown}) => {
     return(
         <OutputContainer>
             <ReactMarkdown>{markdown}</ReactMarkdown>
+            
         </OutputContainer>
     );
 };
